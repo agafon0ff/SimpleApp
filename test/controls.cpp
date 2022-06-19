f39bce8e3a53bcce4276286a44d185bea570031d
Sergey Agafonov <agafon0ff@mail.ru>
2022-06-19 13:07:20 +0200

    Used simple label widget

--- test/controls.cpp
+++ test/controls.cpp
@@ -3,14 +3,15 @@
 #include "controls.h"
 
 Controls::Controls(SA::Widget *parent) : SA::Widget(parent),
-    m_textEdit(new SA::TextEdit(this)),
-    m_lineEdit(new SA::LineEdit(this)),
-    m_buttonAdd(new SA::Button("Add", this))
+    m_textEdit(this),
+    m_lineEdit(this),
+    m_buttonAdd("Add", this),
+    m_label("Text:", this)
 {
     using namespace std::placeholders;
 
-    m_buttonAdd->addPressHandler(std::bind(&Controls::btnAddPressed, this, _1));
-    m_textEdit->setText("    Lorem Ipsum is simply dummy\n"
+    m_buttonAdd.addPressHandler(std::bind(&Controls::btnAddPressed, this, _1));
+    m_textEdit.setText("    Lorem Ipsum is simply dummy\n"
                         "text of the printing and typesetting\n"
                         "industry. Lorem Ipsum has been the\n"
                         "industry's standard dummy text ever\n"
@@ -32,22 +33,20 @@ Controls::Controls(SA::Widget *parent) : SA::Widget(parent),
 
 Controls::~Controls()
 {
-    delete m_buttonAdd;
-    delete m_lineEdit;
-    delete m_textEdit;
     std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
 void Controls::btnAddPressed(bool state)
 {
     if (!state) return;
-    m_textEdit->append(m_lineEdit->text());
-    m_lineEdit->clear();
+    m_textEdit.append(m_lineEdit.text());
+    m_lineEdit.clear();
 }
 
 void Controls::resizeEvent(const SA::Size &size)
 {
-    m_textEdit->setGeometry(5, 5, size.width - 10, size.height - 40);
-    m_buttonAdd->setGeometry(size.width - 105, size.height - 30, 100, 25);
-    m_lineEdit->setGeometry(5, size.height - 30, size.width - 115, 25);
+    m_textEdit.setGeometry(5, 5, size.width - 10, size.height - 40);
+    m_buttonAdd.setGeometry(size.width - 105, size.height - 30, 100, 25);
+    m_lineEdit.setGeometry(60, size.height - 30, size.width - 170, 25);
+    m_label.setGeometry(5, size.height - 30, 55, 25);
 }
